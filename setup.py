2f0b6765092c3bdc4caa16999319f66642c8b4d5
Dimitri Baptiste <escambiadrummer@gmail.com>
2016-05-05 01:42:26 -0500

    created config file, config reader

--- setup.py
+++ setup.py
@@ -1,70 +1,39 @@
-import pickle
+from ConfigParser import ConfigParser as confp
+import sys
+import os
+
+# Constants
+path = sys.path[0]
+config = os.path.join(path, 'setup.cfg')
+
+def channels(reader):
+    chandict = {}
+    # Read in the channel names and passwords, separating them into lists
+    channames = reader.get('channels', 'names').split(",")
+    chanpass = reader.get('channels', 'password').split(",")
+    for a in range(0,len(channames)):
+        chandict[channames[a]] = chanpass[a]
+    return chandict
+
+def server(reader):
+    # Read in the name of the bot and its password
+    bot = reader.get('server', 'botname')
+    botpass = reader.get('server', 'password')
+    return {bot:botpass}
+
+def email(reader):
+    # Read in the email address and password of the user
+    email = reader.get('email', 'address')
+    emailpass = reader.get('email', 'password')
+    return {email:emailpass}
 
 def main():
-    data = {}
-    print('Welcome to startup manager!')
-    print('This setup will create the essential pickle files for Muse-chan to run properly.')
-    print('First, let\'s setup \"config.pickle\", which holds information for Muse-chan to join IRC channels and authenticate herself. Do you want to continue?')
-    answer = input('y/n\n')
-    if not answer == 'n':
-        config()
-
-def config():
-    for a in range(5):
-        print()
-    config = {}
-    name = input('What do you want the bot\'s name to be?\n')
-    password = input('What is its nickserv password?\n')
-    master = input('What is the nick of the bot\'s master? (Your personal nick)\n')
-    email_address = input('What is the email address that the bot will use? (Blank if none)\n')
-    email_password = input('What is the password for the email address? (Blank if none)\n')
-    no_of_channels = int(input('How many channels will the bot join?\n'))
-    config['channels'] = []
-    for a in range(no_of_channels):
-        channel = {}
-        channel_name = input('What is the name of the channel?\n')
-        channel_password = ''
-        channel_password = input('What is the password? (blank if none)\n')
-        channel['name'] = channel_name
-        channel['password'] = channel_password
-        config['channels'].append(channel)
-    for a in range(5):
-        print()
-    print('Please confirm the following details:')
-    print('Name: %s' %(name))
-    print('Password: %s' %(password))
-    print('Master : %s' %(master))
-    print('Email address: %s' %(email_address))
-    print('Email password: %s' %(email_password))
-    for a in config['channels']:
-        print('Channel name: %s, Password: %s' %(a['name'], a['password']))
-    confirmation = input('Is that all? Type \'n\' to restart.\n')
-    if not confirmation == 'n':
-        config['name'] = name
-        config['password'] = password
-        config['master'] = master
-        config['email'] = {}
-        config['email']['address'] = email_address
-        config['email']['password'] = email_password
-        f = open('config.pickle', 'wb')
-        pickle.dump(config,f)
-        f.close()
-        input('Done!')
-        for a in range(5):
-            print()
-        return
-
-
-
-
-
-
-
-
-
+    # Create the config reading object
+    reader = confp()
+    reader.read(config)
+    chan = channels(reader)
+    botserv = server(reader)
+    emailinf = email(reader)
 
 if __name__ == '__main__':
     main()
-
-
-
