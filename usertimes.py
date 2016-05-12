7b77afe73d27b025b39f86fa19b92dbeca344442
SoraSkyy <viorama@gmail.com>
2016-05-12 18:18:53 +0800

    commented code to make it more readable

--- usertimes.py
+++ usertimes.py
@@ -10,6 +10,11 @@ class TimeZoneCheck():
         self.word = self.dict['message']
         self.name = self.dict['name']
         self.dpl = dpl
+
+        #Three types of commands get passed into this class.
+        #$settimezone sets the UTC timezone for the user,
+        #$time <nick> calls the current time at <nick>'s loaction, and
+        #$time calls current time at caller's location.
         if self.word.startswith('$settimezone '):
             return self.set_time(self.name, self.word[13:])
         elif self.word.startswith('$time '):
@@ -18,35 +23,51 @@ class TimeZoneCheck():
             return self.read_time(self.name)
 
     def set_time(self, name, tz):
+        #Brute force technique to allow only valid time zones.
         if tz not in ['-12','-11','-10','-9.5','-9','-8.5','-8','-7','-6','-5','-5','-4','-3.5','-3','-2','-1','0','1','2','3','3','4','4.5','5','5.5','5.75','6','6.5','7','8','8.5','8.75','9','9.5','10','10.5','11','12','12.75','13','14']:
             return 'Invalid Time Zone'
         try:
             timezone = float(tz)
         except:
             return 'Error, that is not a number!'
+
+        #From now on, use timezone instead of 'tz'
+        #UTC timezones cannot be <-12 and >14
         if not ((timezone >= -12) and (timezone <= 14)):
             return 'Error: Invalid time zone.'
+        
+        #Load timezone information into 'data'
         f = open(self.dpl,'rb')
         data = pickle.load(f)
         f.close()
         checktext = '^' + name + '$'
         check = re.compile(checktext, re.IGNORECASE)
+
+        #If user has set his timezone before, replace old timezone with new
+        #Else, create a new entry in the pickle
         for user in data['usertimezones'].keys():
             if check.match(user):
                 data['usertimezones'][user] = timezone
         else:
             data['usertimezones'][name] = timezone
+
+        #Save data with new timezone information
         f = open(self.dpl,'wb')
         pickle.dump(data,f)
         f.close()
+
         return 'Timezone set to '+str(timezone)+', remember to update this when DST starts/ends!'
 
     def read_time(self, name):
+        #Preventing spaces from being included in the 'name'
         name = name.replace(' ','')
+
+        #Load timezone information into 'data'
         f = open(self.dpl,'rb')
         data = pickle.load(f)
         f.close()
-        found = False
+
+        #Making sure that nick does not contain invalid characters
         if len(name) == 0:
             return 'Error: Please enter a user\'s name.'
         if not re.match("^[A-Za-z0-9_\\\[\]{}^`|-]*$", name):
@@ -57,12 +78,18 @@ class TimeZoneCheck():
         name = name.replace('\\', "\\")
         checktext = '\S*' + name + '\S*'
         check = re.compile(checktext, re.IGNORECASE)
+
+        #Begin search for nick
+        found = False
         for user in data['usertimezones'].keys():
             if check.match(user):
                 found = True
                 timezone = data['usertimezones'][user]
                 time = datetime.datetime.utcnow() + datetime.timedelta(hours=timezone)
                 weekday = days[time.weekday()]
+
+                #These ensure that the time format comes out as 09:05
+                #instead of 9:5
                 if time.hour < 10:
                     hour_zero = str(0)
                 else:
@@ -71,6 +98,9 @@ class TimeZoneCheck():
                     minute_zero = str(0)
                 else:
                     minute_zero = ''
+
+                #This ensures that names ending with 's' get an apostrophe
+                #and names that do not will get 's
                 if user[-1] == 's':
                     user = str(user) + '\''
                 else:
@@ -79,6 +109,9 @@ class TimeZoneCheck():
         if found == False:
             return 'Either this person has not registered, or such person does not exist.'
 
+
+    #This function is used by logger.py that gives the raw timezone number
+    #for log-reading
     def get_raw_timezone(self,dpl,name):
         self.dpl = dpl
         f = open(self.dpl,'rb')
