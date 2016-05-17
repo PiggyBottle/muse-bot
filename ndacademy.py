7cdd905927e5ca243271cf829605c25c44e6ea27
SoraSkyy <viorama@gmail.com>
2016-05-17 18:19:56 +0800

    almost forgot to add ndacademy.py

---
+++ ndacademy.py
@@ -0,0 +1,14 @@
+import pickle
+
+
+
+
+
+
+class NDAcademy():
+    def __init__(self,emailer,ndapl):
+        self.emailer = emailer
+        self.ndapl = ndapl
+
+    def execute(self):
+        pass
